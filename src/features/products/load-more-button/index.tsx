3ec38e6405f2b52390f4a20b07660735c256768e
zlx454545 <zlx454545@gmail.com>
2024-04-10 03:12:06 +0900

    feat: load more button 컴포넌트 구현

---
+++ src/features/products/load-more-button/index.tsx
@@ -0,0 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../../store';
+import { loadMoreProducts } from '../products-slice';
+
+const LoadMoreButton = () => {
+  const isMaximum = useSelector((state: RootState) => {
+    const { skip, total } = state.products;
+    return total && total <= skip;
+  });
+  const dispatch = useDispatch<AppDispatch>();
+
+  if (isMaximum) return null;
+
+  return <button onClick={() => dispatch(loadMoreProducts())}>더 보기</button>;
+};
+export default LoadMoreButton;
