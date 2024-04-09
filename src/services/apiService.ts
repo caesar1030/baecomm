b51c4bc684425d76a33605c41f1af94bb44ff3e0
zlx454545 <zlx454545@gmail.com>
2024-04-10 03:49:17 +0900

    feat: 검색 api service 구현

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -8,3 +8,12 @@ export const fetchProductsApi = async (limit: number, skip: number) => {
 
   return data as TProductsApiResponse;
 };
+
+export const fetchProductsBySearchApi = async (searchTerm: string) => {
+  const res = await fetch(
+    `https://dummyjson.com/products/search?q=${searchTerm}`
+  );
+  const data = await res.json();
+
+  return data as TProductsApiResponse;
+};
