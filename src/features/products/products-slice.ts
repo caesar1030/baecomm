de559975387a8365a52fedbac3d739e7da778f78
zlx454545 <zlx454545@gmail.com>
2024-04-10 03:09:28 +0900

    feat: product slice 기능 구현

---
+++ src/features/products/products-slice.ts
@@ -0,0 +1,60 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { TProduct } from '../../types/product';
+import { fetchProductsApi } from '../../services/apiService';
+import { TProductsApiResponse } from '../../types/api-response';
+import { RootState } from '../../store';
+
+const LIMIT = 10;
+
+const initialState: {
+  products: TProduct[];
+  total: number | null;
+  skip: number;
+} = {
+  products: [],
+  total: null,
+  skip: 0,
+};
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadInitialProducts.fulfilled, (state, action) => {
+        const { products, skip, total } = action.payload;
+        state.products = products;
+        state.total = total;
+        state.skip = skip + LIMIT;
+      })
+      .addCase(loadMoreProducts.fulfilled, (state, action) => {
+        const { products, skip, total } = action.payload;
+        state.products = [...state.products, ...products];
+        state.total = total;
+        state.skip = skip + LIMIT;
+      });
+  },
+});
+
+export const loadInitialProducts = createAsyncThunk(
+  'products/loadInitialProducts',
+  async () => {
+    const data = await fetchProductsApi(LIMIT, 0);
+    return data;
+  }
+);
+
+export const loadMoreProducts = createAsyncThunk<
+  TProductsApiResponse,
+  void,
+  { state: RootState }
+>('products/loadMoreProducts', async (_, { getState }) => {
+  const state = getState();
+  const { skip } = state.products;
+  const data = await fetchProductsApi(LIMIT, skip);
+
+  return data;
+});
+
+export default productsSlice.reducer;
